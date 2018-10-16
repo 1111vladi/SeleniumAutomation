c2789484a4db98862cb46fab2b73d2661e9d5ff6
1111vladi <vladiasd8@gmail.com>
2018-10-17 01:06:50 +0300

    Simple Selenium Automation

---
+++ src/ProggioCreateProject.java
@@ -0,0 +1,33 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ProggioCreateProject {
+
+    public static void main(String[] args) {
+
+        // Basic set up to use Chrome browser
+        System.setProperty("webdriver.chrome.driver", "E:\\Program Files (x86)\\chromedriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        // Navigate to the specified url
+        driver.get("https://app.proggio.com/login");
+
+        // Finding element by id and sendKey(input) new value
+        WebElement emailElement = driver.findElement(By.id("input-email"));
+        WebElement passwordElement = driver.findElement(By.id("input-password"));
+        emailElement.sendKeys("vladiasd8@gmail.com");
+        passwordElement.sendKeys("130819933zzxx\n");
+
+        // WebDriverWait required
+        // Figured out I need to "wait" between web reloads
+        // "Create Project" and "New Project" by class/cssSelector
+        WebElement createProject = driver.findElement(By.cssSelector("input[class='sc-bdVaJa laHbOK]"));
+        createProject.click();
+
+        driver.quit();
+    }
+
+}
